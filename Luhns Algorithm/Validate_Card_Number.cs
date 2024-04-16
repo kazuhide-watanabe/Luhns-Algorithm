810c3b4bd1f0e9934785058f57fef27fde4c32f8
Kazuhide Watanabe <watanake@mail.uc.edu>
2024-04-16 12:03:05 -0400

    Updated with a new class that validates the credit card number structurally.

---
+++ Luhns Algorithm/Validate_Card_Number.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luhns_Algorithm
+{
+    internal class Validate_Card_Number
+    {
+        public static bool IsCardNumberValid(string cardNumber)
+        {
+            int i, checkSum = 0;
+
+            for (i = cardNumber.Length - 1; i >= 0; i -= 2)
+                checkSum += (cardNumber[i] - '0');
+
+            for (i = cardNumber.Length - 2; i >= 0; i -= 2)
+            {
+                int val = ((cardNumber[i] - '0') * 2);
+                while (val > 0)
+                {
+                    checkSum += (val % 10);
+                    val /= 10;
+                }
+            }
+
+            return ((checkSum % 10) == 0);
+        }
+    }
+}
